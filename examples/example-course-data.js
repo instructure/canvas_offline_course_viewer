d13b0ca72c536a8854b2f6dd1cd3017ef70d823e
Mysti Sadler <mysti@instructure.com>
2017-01-16 16:37:51 -0700

    Makes files listing
    closes OFFW-78
    
    Test plan
    - Load app, you should be in the files view
    - You should see the contents of the top level folder
      from the example_course_data.js files listing
    - The folders pane is empty for now
    
    Change-Id: I1eeea4b5a84f59641e344b91e464e8cd4895cb9e
    Reviewed-on: https://gerrit.instructure.com/99731
    Reviewed-by: Cameron Sutter <csutter@instructure.com>
    Product-Review: Cameron Sutter <csutter@instructure.com>
    Tested-by: Jenkins
    QA-Review: Nathan Rogowski <nathan@instructure.com>

--- examples/example-course-data.js
+++ examples/example-course-data.js
@@ -2,4 +2,35 @@ window.COURSE_DATA = {
   language: 'en',
   lastDownload: 'Nov 22, 2016 at 10:33 AM',
   title: 'Example Course',
+  "files":
+  [
+    {
+      "type":"file",
+      "name":"MS-XAML.pdf",
+      "size":2931726,
+      "files":null
+    },
+    {
+      "type":"folder",
+      "name":"Folder 1",
+      "size":null,
+      "files":
+      [
+        {
+          "type":"folder",
+          "name":"Folder 2",
+          "size":null,
+          "files":
+          [
+            {
+              "type":"file",
+              "name":"1.2 Speed Classwork.docx",
+              "size":125270,
+              "files":null
+            }
+          ]
+        }
+      ]
+    }
+  ]
 }
