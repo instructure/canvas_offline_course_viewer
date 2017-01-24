01fbb615230948dad50e93f6512715365c6935c6
Jon Willesen <jonw@jonw-l.corp.instructure.com>
2017-01-24 09:52:25 -0700

    add i18n:check to the build process
    closes OFFW-91
    
    test plan: build passes
    
    Change-Id: Id075f4e6a93599a04aa67184e02dd9da958412ea
    Reviewed-on: https://gerrit.instructure.com/100348
    Tested-by: Jenkins
    Reviewed-by: Mysti Sadler <mysti@instructure.com>
    Product-Review: Jon Willesen <jonw+gerrit@instructure.com>
    QA-Review: Jon Willesen <jonw+gerrit@instructure.com>

--- build.sh
+++ build.sh
@@ -1,8 +1,19 @@
 set -ex
 
 docker-compose build
-docker-compose run --rm -T test npm run lint
-docker-compose run --rm -T test npm run test:coverage
+docker-compose run --rm test npm run lint
+docker-compose run --rm test npm run test:coverage
+
+# i18nliner always returns success, so grep output for failures
+docker-compose run --rm test npm run i18n:check | tee check.txt
+if egrep '\b0 failure' check.txt ; then
+  true
+else
+  if egrep '\d+ failure' check.txt ; then
+    false
+  fi
+fi
+
 docker-compose up -d
 
 docker cp $(docker-compose ps -q test-data):/usr/src/app/coverage/. coverage
