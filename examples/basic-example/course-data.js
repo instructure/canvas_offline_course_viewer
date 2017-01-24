9a961507418341cdd5f8ba7691c699eab9157613
Mysti Sadler <mysti@instructure.com>
2017-01-24 16:20:14 -0700

    Module export structure
    refs OFFW-79
    
    Change-Id: I34426b21e05b79227051b2ad70c6ee1c1e42ad59
    Reviewed-on: https://gerrit.instructure.com/100402
    Reviewed-by: Cameron Sutter <csutter@instructure.com>
    Product-Review: Cameron Sutter <csutter@instructure.com>
    QA-Review: Cameron Sutter <csutter@instructure.com>
    Tested-by: Jenkins

--- examples/basic-example/course-data.js
+++ examples/basic-example/course-data.js
@@ -44,5 +44,100 @@ window.COURSE_DATA = {
         }
       ]
     }
-  ]
+  ],
+  "modules":[
+    {
+      "id":"crazy_string_of_doom",
+      "name":"Module 1",
+      "locked":false,
+      "unlockDate":null,
+      "prereq":null,
+      "items":[
+        {
+          "title":"Module 1 Item 1",
+          "type":"assignment",
+          "completed":true,
+          "pointsPossible":10,
+          "indent":0,
+          "requirement":"submit",
+          "requiredPoints":null,
+          "description":"<p>Hi</p>",
+          "locked":false,
+        },
+        {
+          "title":"Module 1 Item 2",
+          "type":"quiz",
+          "completed":false,
+          "pointsPossible":5,
+          "indent":1,
+          "requirement":"score",
+          "requiredPoints":4,
+          "description":"<p>Hi</p>",
+          "locked":false,
+        },
+      ],
+    },
+    {
+      "id":"crazy_string2",
+      "name":"Module 2",
+      "locked":true,
+      "unlockDate":"2017-05-05T00:00:00Z",
+      "prereq":null,
+      "items":[
+        {
+          "title":"Module 2 Item 1",
+          "type":"discussion",
+          "completed":false,
+          "pointsPossible":null,
+          "indent":0,
+          "requiment":"view",
+          "requiredPoints":null,
+          "description":"<p>Hi</p>",
+          "locked":true,
+        },
+        {
+          "title":"Module 2 Item 2",
+          "type":"text",
+          "completed":null,
+          "indent":0,
+          "locked":true,
+        },
+        {
+          "title":"Module 2 Item 3",
+          "type":"url",
+          "completed":false,
+          "indent":5,
+          "requirement":"view",
+          "url":"www.google.com",
+          "locked":true,
+        },
+        {
+          "title":"Module 2 Item 4",
+          "type":"tool",
+          "completed":false,
+          "indent":0,
+          "requirement":"view",
+          "locked":true,
+        },
+      ],
+    },
+    {
+      "id":"crazy_string3",
+      "name":"Module 3",
+      "locked":true,
+      "unlockeDate":null,
+      "prereq":"crazy_string_of_doom",
+      "items":[
+        {
+          "title":"Module 3 Item 1",
+          "type":"file",
+          "completed":false,
+          "indent":0,
+          "requirement":"view",
+          "file":"dist/viewer/files/file1.jpg",
+          "locked":true,
+        },
+      ],
+    },
+  ],
 }
