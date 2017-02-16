10d1e7060dd96bf85805cfed47ba61ade26e3336
Mysti Sadler <mysti@instructure.com>
2017-02-15 17:56:34 -0700

    Updated quiz and discussion attributes in examples
    refs OFFW-112
    
    Test plan
    - See https://gerrit.instructure.com/#/c/102371/
    
    Change-Id: I420a45276659726e8f67d00ad43417deb9a38072
    Reviewed-on: https://gerrit.instructure.com/102372
    Tested-by: Jenkins
    Reviewed-by: Jon Willesen <jonw+gerrit@instructure.com>
    Product-Review: Mysti Sadler <mysti@instructure.com>
    QA-Review: Mysti Sadler <mysti@instructure.com>

--- examples/basic-example/course-data.js
+++ examples/basic-example/course-data.js
@@ -1,199 +1,301 @@
 window.COURSE_DATA = {
-  language: 'en',
-  lastDownload: 'Nov 22, 2016 at 10:33 AM',
-  title: 'Example Course',
-  "files":
-  [
+  "language": "en",
+  "lastDownload": "2017-02-16T13:02:38-07:00",
+  "title": "Test Course 1",
+  "files": [
     {
-      "type":"file",
-      "name":"test.pdf",
-      "size":2931726,
-      "files":null
-    },
-    {
-      "type":"file",
-      "name":"test.txt",
-      "size":2931726,
-      "files":null
-    },
-    {
-      "type":"file",
-      "name":"test.bin",
-      "size":2931726,
-      "files":null
-    },
-    {
-      "type":"folder",
-      "name":"folder1",
-      "size":null,
-      "files":
-      [
+      "type": "folder",
+      "name": "Folder#1",
+      "size": null,
+      "files": [
         {
-          "type":"folder",
-          "name":"folder2",
-          "size":null,
-          "files":
-          [
+          "type": "file",
+          "name": "!@#$%^&*().txt",
+          "size": 22,
+          "files": null
+        },
+        {
+          "type": "folder",
+          "name": "folder 1",
+          "size": null,
+          "files": [
             {
-              "type":"file",
-              "name":"test.txt",
-              "size":125270,
-              "files":null
+              "type": "file",
+              "name": "images.jpg",
+              "size": 3332,
+              "files": null
+            }
+          ]
+        },
+        {
+          "type": "folder",
+          "name": "Folder 2",
+          "size": null,
+          "files": [
+            {
+              "type": "file",
+              "name": "test.pdf",
+              "size": 51003,
+              "files": null
             }
           ]
         }
       ]
     },
     {
-      "type":"folder",
-      "name":"top_folder_2",
-      "size":null,
-      "files":
-      [
+      "type": "file",
+      "name": "music-box.mp4",
+      "size": 580724,
+      "files": null
+    }
+  ],
+  "modules": [
+    {
+      "id": 2,
+      "name": "Module 1",
+      "status": "completed",
+      "unlockDate": "2016-11-30T00:00:00-07:00",
+      "prereqs": [],
+      "requirement": "all",
+      "sequential": false,
+      "items": [
         {
-          "type":"file",
-          "name":"test.txt",
-          "size":1234,
-          "files":null
+          "id": 3,
+          "title": "Assignment 1",
+          "type": "Assignment",
+          "indent": 0,
+          "submissionTypes": "a text entry box or a file upload",
+          "graded": true,
+          "pointsPossible": 10.0,
+          "dueAt": "2017-02-08T23:59:59-07:00",
+          "lockAt": "2017-02-15T23:59:59-07:00",
+          "unlockAt": "2017-02-06T00:00:00-07:00",
+          "requirement": "must_mark_done",
+          "locked": false,
+          "completed": true,
+          "content": "<p><a id=\"\" class=\"\" title=\"\" href=\"http://www.google.com\" target=\"\">Google</a></p>\r\n<p>"
+        },
+        {
+          "id": 14,
+          "title": "Google",
+          "type": "ExternalUrl",
+          "indent": 1,
+          "requirement": null,
+          "locked": false,
+          "completed": false,
+          "content": "https://www.google.com"
+        },
+        {
+          "id": 15,
+          "title": "User Validation",
+          "type": "ContextExternalTool",
+          "indent": 0,
+          "requirement": null,
+          "locked": false,
+          "completed": false,
+          "content": null
+        },
+        {
+          "id": 22,
+          "title": "Quiz 2",
+          "type": "Quizzes::Quiz",
+          "indent": 0,
+          "questionCount": 0,
+          "timeLimit": 3,
+          "attempts": -1,
+          "graded": true,
+          "pointsPossible": 0.0,
+          "dueAt": null,
+          "lockAt": null,
+          "unlockAt": null,
+          "requirement": "min_score",
+          "requiredPoints": 4.0,
+          "locked": false,
+          "completed": true,
+          "content": ""
+        },
+        {
+          "id": 24,
+          "title": "Assignment 2",
+          "type": "Assignment",
+          "indent": 0,
+          "submissionTypes": "a text entry box or a file upload",
+          "graded": true,
+          "pointsPossible": null,
+          "dueAt": null,
+          "lockAt": null,
+          "unlockAt": null,
+          "requirement": "must_submit",
+          "locked": false,
+          "completed": true,
+          "content": ""
+        },
+        {
+          "id": 25,
+          "title": "Page 1",
+          "type": "WikiPage",
+          "indent": 0,
+          "requirement": null,
+          "locked": false,
+          "completed": false,
+          "content": "<p style=\"background-image: file.jpg;\">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Why do we use it? It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>"
+        },
+        {
+          "id": 33,
+          "title": "Not graded assignment",
+          "type": "Assignment",
+          "indent": 0,
+          "submissionTypes": null,
+          "graded": false,
+          "dueAt": null,
+          "lockAt": null,
+          "unlockAt": null,
+          "requirement": null,
+          "locked": false,
+          "completed": false,
+          "content": ""
+        },
+        {
+          "id": 34,
+          "title": "No Submission",
+          "type": "Assignment",
+          "indent": 0,
+          "submissionTypes": null,
+          "graded": true,
+          "pointsPossible": 0.0,
+          "dueAt": null,
+          "lockAt": null,
+          "unlockAt": null,
+          "requirement": null,
+          "locked": false,
+          "completed": false,
+          "content": ""
+        },
+        {
+          "id": 35,
+          "title": "!@#$%^&*().txt",
+          "type": "Attachment",
+          "indent": 0,
+          "requirement": null,
+          "locked": false,
+          "completed": false,
+          "content": "Test-Course-1-2017-Feb-16_20-02-49/viewer/files//Folder#1/1485469559_710__!@#$%^&*().txt"
+        }
+      ]
+    },
+    {
+      "id": 6,
+      "name": "Module 2",
+      "status": "locked",
+      "unlockDate": "2017-03-31T00:00:00-06:00",
+      "prereqs": [],
+      "requirement": "all",
+      "sequential": true,
+      "items": [
+        {
+          "id": 17,
+          "title": "Text Header",
+          "type": "ContextModuleSubHeader",
+          "indent": 0
+        },
+        {
+          "id": 18,
+          "title": "Quiz 1",
+          "type": "Quizzes::Quiz",
+          "indent": 0,
+          "questionCount": 0,
+          "timeLimit": null,
+          "attempts": 1,
+          "graded": true,
+          "pointsPossible": 0.0,
+          "dueAt": null,
+          "lockAt": null,
+          "unlockAt": null,
+          "requirement": "min_score",
+          "requiredPoints": 4.0,
+          "locked": true,
+          "completed": false
+        },
+        {
+          "id": 30,
+          "title": "Graded Survey",
+          "type": "Quizzes::Quiz",
+          "indent": 0,
+          "questionCount": 1,
+          "timeLimit": null,
+          "attempts": 1,
+          "graded": true,
+          "pointsPossible": null,
+          "dueAt": null,
+          "lockAt": null,
+          "unlockAt": null,
+          "requirement": null,
+          "locked": true,
+          "completed": false
+        },
+        {
+          "id": 31,
+          "title": "Practice Quiz",
+          "type": "Quizzes::Quiz",
+          "indent": 0,
+          "questionCount": 0,
+          "timeLimit": null,
+          "attempts": 1,
+          "graded": true,
+          "pointsPossible": 0.0,
+          "dueAt": null,
+          "lockAt": null,
+          "unlockAt": null,
+          "requirement": null,
+          "locked": true,
+          "completed": false
+        },
+        {
+          "id": 32,
+          "title": "Ungraded Survey",
+          "type": "Quizzes::Quiz",
+          "indent": 0,
+          "questionCount": 0,
+          "timeLimit": null,
+          "attempts": 1,
+          "graded": false,
+          "dueAt": null,
+          "lockAt": null,
+          "unlockAt": null,
+          "requirement": null,
+          "locked": true,
+          "completed": false
+        }
+      ]
+    },
+    {
+      "id": 7,
+      "name": "Module 3",
+      "status": "locked",
+      "unlockDate": null,
+      "prereqs": [
+        2,
+        6
+      ],
+      "requirement": "one",
+      "sequential": false,
+      "items": [
+        {
+          "id": 26,
+          "title": "Discussion 2",
+          "type": "DiscussionTopic",
+          "indent": 0,
+          "graded": true,
+          "pointsPossible": null,
+          "dueAt": null,
+          "lockAt": null,
+          "unlockAt": null,
+          "requirement": "min_score",
+          "requiredPoints": 4.0,
+          "locked": true,
+          "completed": false
         }
       ]
     }
-  ],
-  "modules":[
-    {
-      "id":1,
-      "name":"Module 1",
-      "status":"started",
-      "unlockDate":null,
-      "prereqs":null,
-      "requirement":"all",
-      "sequential":false,
-      "items":[
-        {
-          "id":1,
-          "title":"Module 1 Item 1",
-          "type":"Assignment",
-          "indent":0,
-          "locked":false,
-          "completed":true,
-          "pointsPossible":0,
-          "requirement":"must_submit",
-          "dueAt":"2017-02-06T15:07:00Z",
-          "unlockAt":"2017-02-05T15:07:00Z",
-          "lockAt":"2017-02-18T15:07:00Z",
-          "submissionTypes":"online_text_entry,online_upload",
-          "content":"<p>Hi</p>",
-        },
-        {
-          "id":2,
-          "title":"Module 1 Item 2",
-          "type":"Quizzes::Quiz",
-          "indent":1,
-          "locked":false,
-          "completed":true,
-          "pointsPossible":5,
-          "requirement":"min_score",
-          "requiredPoints":4.0,
-          "dueAt":"2017-02-06T15:07:00Z",
-          "unlockAt":"2017-02-05T15:07:00Z",
-          "lockAt":"2017-02-18T15:07:00Z",
-          "questionCount":5,
-          "content":"<p>Hi</p>",
-        },
-        {
-          "id":5,
-          "title":"Module 1 Item 3",
-          "type":"ExternalUrl",
-          "indent":5,
-          "locked":false,
-          "completed":true,
-          "requirement":"must_view",
-          "content":"www.google.com",
-        },
-        {
-          "id":9,
-          "title":"Module 1 Item 4",
-          "type":"Attachment",
-          "indent":0,
-          "locked":false,
-          "completed":true,
-          "requirement":"must_view",
-          "content":"content-package-name-date/viewer/files/file 1.jpg"
-        },
-        {
-          "id":10,
-          "title":"Module 1 Item 5",
-          "type":"WikiPage",
-          "indent":0,
-          "locked":false,
-          "completed":false,
-          "requirement":"must_contribute",
-          "content":"<p>YoYo</p>"
-        },
-        {
-          "id":3,
-          "title":"Module 2 Item 1",
-          "type":"DiscussionTopic",
-          "indent":0,
-          "locked":false,
-          "pointsPossible":null,
-          "requirement":"must_contribute",
-          "content":"<p>Hi</p>",
-        },
-      ],
-    },
-    {
-      "id":2,
-      "name":"Module 2",
-      "status":"locked",
-      "unlockDate":"2017-05-05T00:00:00Z",
-      "prereqs":null,
-      "requirement":null,
-      "sequential":false,
-      "items":[
-        {
-          "id":4,
-          "title":"Module 2 Item 2",
-          "type":"ContextModuleSubHeader",
-          "indent":0,
-          "locked":true,
-        },
-        {
-          "id":6,
-          "title":"Module 2 Item 4",
-          "type":"ContextExternalTool",
-          "indent":0,
-          "locked":true,
-          "requirement":"must_view",
-        },
-      ],
-    },
-    {
-      "id":3,
-      "name":"Module 3",
-      "status":"locked",
-      "unlockDate":null,
-      "prereqs":[1,2],
-      "requirement":"one",
-      "sequential":true,
-      "items":[
-        {
-          "id":7,
-          "title":"Module 3 Item 1",
-          "type":"Attachment",
-          "indent":0,
-          "locked":true,
-          "requirement":"must_view",
-        },
-        {
-          "id":8,
-          "title":"Module 3 Item 2",
-          "type":"Assignment",
-          "indent":0,
-          "locked":true,
-          "requirement":"must_mark_done",
-        },
-      ],
-    },
-  ],
+  ]
 }
