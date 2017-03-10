c90f4cab4b68bc82cc7d97918da298e451d6ac0a
Mysti Sadler <mysti@instructure.com>
2017-03-09 19:59:46 -0700

    Fix internal linking of module items
    fixes OFFW-127
    
    Test plan
    - See https://gerrit.instructure.com/#/c/104678/
    
    Change-Id: I0477f0f24f079f5be1fa11f5800c52590ed5b051
    Reviewed-on: https://gerrit.instructure.com/104679
    Tested-by: Jenkins
    Reviewed-by: Jon Willesen <jonw+gerrit@instructure.com>
    QA-Review: Nathan Rogowski <nathan@instructure.com>
    Product-Review: Mysti Sadler <mysti@instructure.com>

--- examples/basic-example/course-data.js
+++ examples/basic-example/course-data.js
@@ -1,6 +1,6 @@
 window.COURSE_DATA = {
   "language": "en",
-  "lastDownload": "2017-02-16T13:02:38-07:00",
+  "lastDownload": "2017-03-09T19:45:32-07:00",
   "title": "Test Course 1",
   "files": [
     {
@@ -51,55 +51,58 @@ window.COURSE_DATA = {
   ],
   "modules": [
     {
-      "id": 2,
+      "id": 1,
       "name": "Module 1",
-      "status": "completed",
-      "unlockDate": "2016-11-30T00:00:00-07:00",
+      "status": "unlocked",
+      "unlockDate": null,
       "prereqs": [],
       "requirement": "all",
       "sequential": false,
+      "exportId": "i90fd95d3f73a0ca27fe13e89c068f973",
       "items": [
         {
-          "id": 3,
+          "id": 1,
           "title": "Assignment 1",
           "type": "Assignment",
           "indent": 0,
+          "locked": false,
           "submissionTypes": "a text entry box or a file upload",
           "graded": true,
           "pointsPossible": 10.0,
           "dueAt": "2017-02-08T23:59:59-07:00",
-          "lockAt": "2017-02-15T23:59:59-07:00",
+          "lockAt": "2017-03-31T23:59:00-06:00",
           "unlockAt": "2017-02-06T00:00:00-07:00",
           "requirement": "must_mark_done",
-          "locked": false,
-          "completed": true,
-          "content": "<p><a id=\"\" class=\"\" title=\"\" href=\"http://www.google.com\" target=\"\">Google</a></p>\r\n<p>"
+          "completed": false,
+          "content": "<p><a id=\"\" class=\"\" title=\"\" href=\"http://www.google.com\" target=\"\">Google</a></p>\r\n<p><a id=\"\" class=\"\" title=\"Page 1\" href=\"pages/page-1\" target=\"\" data-api-endpoint=\"http://localhost:3000/api/v1$WIKI_REFERENCE$/pages/page-1\" data-api-returntype=\"Page\">Page 1</a></p>\r\n<p><a id=\"\" class=\" instructure_image_thumbnail instructure_file_link\" title=\"2-7671.jpg\" href=\"http://localhost:3000/courses/1/files/86/download?wrap=1\" target=\"\" data-api-endpoint=\"http://localhost:3000/api/v1/courses/1/files/86\" data-api-returntype=\"File\">2-7671.jpg</a></p>\r\n<p><a id=\"\" class=\" instructure_file_link\" title=\"!@#$%^&amp;*().txt\" href=\"viewer/files/Folder%231/!@%23%24%25%5E&amp;*().txt?canvas_download=1&amp;canvas_qs_wrap=1\" target=\"\" data-api-endpoint=\"http://localhost:3000/api/v1$IMS-CC-FILEBASE$/Folder%231/!@%23$%25%5E&amp;*().txt\" data-api-returntype=\"File\">!@#$%^&amp;*().txt</a></p>\r\n<p><a id=\"\" class=\" instructure_image_thumbnail instructure_file_link\" title=\"images.jpg\" href=\"viewer/files/Folder%231/folder%201/images.jpg?canvas_download=1&amp;canvas_qs_wrap=1\" target=\"\" data-api-endpoint=\"http://localhost:3000/api/v1$IMS-CC-FILEBASE$/Folder%231/folder%201/images.jpg\" data-api-returntype=\"File\">images.jpg</a></p>\r\n<p><img src=\"http://weknowyourdreams.com/images/kitten/kitten-01.jpg\" alt=\"Kitten on windowsill\" width=\"500\" height=\"350\"></p>\r\n<p><img class=\"equation_image\" title=\"\\int^{\\infty}_1\\frac{6x^2+3x}{x^2+x}\" src=\"http://localhost:3000/equation_images/%255Cint%255E%257B%255Cinfty%257D_1%255Cfrac%257B6x%255E2%2B3x%257D%257Bx%255E2%2Bx%257D\" alt=\"\\int^{\\infty}_1\\frac{6x^2+3x}{x^2+x}\" data-mathml='&lt;math xmlns=\"http://www.w3.org/1998/Math/MathML\" display=\"inline\"&gt;&lt;msubsup&gt;&lt;mo&gt;&amp;Integral;&lt;/mo&gt;&lt;mn&gt;1&lt;/mn&gt;&lt;mrow&gt;&lt;mn&gt;&amp;infin;&lt;/mn&gt;&lt;/mrow&gt;&lt;/msubsup&gt;&lt;mfrac&gt;&lt;mrow&gt;&lt;mn&gt;6&lt;/mn&gt;&lt;msup&gt;&lt;mi&gt;x&lt;/mi&gt;&lt;mn&gt;2&lt;/mn&gt;&lt;/msup&gt;&lt;mo&gt;+&lt;/mo&gt;&lt;mn&gt;3&lt;/mn&gt;&lt;mi&gt;x&lt;/mi&gt;&lt;/mrow&gt;&lt;mrow&gt;&lt;msup&gt;&lt;mi&gt;x&lt;/mi&gt;&lt;mn&gt;2&lt;/mn&gt;&lt;/msup&gt;&lt;mo&gt;+&lt;/mo&gt;&lt;mi&gt;x&lt;/mi&gt;&lt;/mrow&gt;&lt;/mfrac&gt;&lt;/math&gt;'></p>\r\n<p><a id=\"media_comment_m-4U6PQnBvPtqVv5WHFKpM8oqrNWjShrgn\" class=\" instructure_video_link instructure_file_link\" title=\"music-box.mp4\" href=\"viewer/files/music-box.mp4?canvas_download=1&amp;canvas_qs_wrap=1\" target=\"\" data-api-endpoint=\"http://localhost:3000/api/v1$IMS-CC-FILEBASE$/music-box.mp4\" data-api-returntype=\"File\">music-box.mp4</a></p>\r\n<p><a id=\"\" class=\" instructure_file_link\" title=\"test.pdf\" href=\"viewer/files/Folder%231/Folder%202/test.pdf?canvas_download=1&amp;canvas_qs_wrap=1\" target=\"\" data-api-endpoint=\"http://localhost:3000/api/v1$IMS-CC-FILEBASE$/Folder%231/Folder%202/test.pdf\" data-api-returntype=\"File\">test.pdf</a></p>\r\n<p><a id=\"\" class=\"\" title=\"Linked Page\" href=\"pages/linked-page\" target=\"\" data-api-endpoint=\"http://localhost:3000/api/v1$WIKI_REFERENCE$/pages/linked-page\" data-api-returntype=\"Page\">Linked Page</a></p>",
+          "exportId": "ia2a35a650dfbfe6d08f0c3e04b55ca17"
         },
         {
-          "id": 14,
+          "id": 2,
           "title": "Google",
           "type": "ExternalUrl",
           "indent": 1,
-          "requirement": null,
           "locked": false,
+          "requirement": null,
           "completed": false,
           "content": "https://www.google.com"
         },
         {
-          "id": 15,
+          "id": 3,
           "title": "User Validation",
           "type": "ContextExternalTool",
           "indent": 0,
-          "requirement": null,
           "locked": false,
+          "requirement": null,
           "completed": false,
           "content": null
         },
         {
-          "id": 22,
+          "id": 19,
           "title": "Quiz 2",
           "type": "Quizzes::Quiz",
           "indent": 0,
+          "locked": false,
           "questionCount": 0,
           "timeLimit": 3,
           "attempts": -1,
@@ -110,56 +113,60 @@ window.COURSE_DATA = {
           "unlockAt": null,
           "requirement": "min_score",
           "requiredPoints": 4.0,
-          "locked": false,
-          "completed": true,
-          "content": ""
+          "completed": false,
+          "content": "",
+          "exportId": "ide4d4fe237ffa50180563f81df123832"
         },
         {
-          "id": 24,
+          "id": 4,
           "title": "Assignment 2",
           "type": "Assignment",
           "indent": 0,
+          "locked": false,
           "submissionTypes": "a text entry box or a file upload",
           "graded": true,
-          "pointsPossible": null,
+          "pointsPossible": 0.0,
           "dueAt": null,
           "lockAt": null,
           "unlockAt": null,
           "requirement": "must_submit",
-          "locked": false,
-          "completed": true,
-          "content": ""
+          "completed": false,
+          "content": "<p><a id=\"\" class=\"\" title=\"Page 1\" href=\"pages/page-1\" target=\"\" data-api-endpoint=\"http://localhost:3000/api/v1$WIKI_REFERENCE$/pages/page-1\" data-api-returntype=\"Page\">Page 1</a></p>\r\n<p><a id=\"\" class=\"\" title=\"Assignment 1\" href=\"assignments/ia2a35a650dfbfe6d08f0c3e04b55ca17\" target=\"\" data-api-endpoint=\"http://localhost:3000/api/v1$CANVAS_OBJECT_REFERENCE$/assignments/ia2a35a650dfbfe6d08f0c3e04b55ca17\" data-api-returntype=\"Assignment\">Assignment 1</a></p>\r\n<p><a id=\"\" class=\"\" title=\"Quiz 2\" href=\"quizzes/ide4d4fe237ffa50180563f81df123832\" target=\"\">Quiz 2</a></p>\r\n<p><a id=\"\" class=\"\" title=\"Announcements List\" href=\"announcements\" target=\"\">Announcements List</a></p>\r\n<p><a id=\"\" class=\"\" title=\"Discussion 3\" href=\"discussion_topics/icf39d29fd7924bb6b2ede5bfa34403f7\" target=\"\" data-api-endpoint=\"http://localhost:3000/api/v1$CANVAS_OBJECT_REFERENCE$/discussion_topics/icf39d29fd7924bb6b2ede5bfa34403f7\" data-api-returntype=\"Discussion\">Discussion 3</a></p>\r\n<p><a id=\"\" class=\"\" title=\"Module 1\" href=\"modules/i90fd95d3f73a0ca27fe13e89c068f973\" target=\"\" data-api-endpoint=\"http://localhost:3000/api/v1$CANVAS_OBJECT_REFERENCE$/modules/i90fd95d3f73a0ca27fe13e89c068f973\" data-api-returntype=\"Module\">Module 1</a></p>\r\n<p><a id=\"\" class=\"\" title=\"Modules List\" href=\"modules\" target=\"\" data-api-endpoint=\"http://localhost:3000/api/v1$CANVAS_COURSE_REFERENCE$/modules\" data-api-returntype=\"[Module]\">Modules List</a></p>\r\n<p><a id=\"\" class=\"\" title=\"Files Index\" href=\"files\" target=\"\" data-api-endpoint=\"http://localhost:3000/api/v1/courses/1/folders/root\" data-api-returntype=\"Folder\">Files Index</a></p>\r\n<p><a id=\"\" class=\"\" title=\"Grades\" href=\"grades\" target=\"\">Grades</a></p>",
+          "exportId": "i2102a7fa93b29226774949298626719d"
         },
         {
-          "id": 25,
+          "id": 5,
           "title": "Page 1",
           "type": "WikiPage",
           "indent": 0,
-          "requirement": null,
           "locked": false,
+          "requirement": null,
           "completed": false,
-          "content": "<p style=\"background-image: file.jpg;\">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Why do we use it? It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>"
+          "content": "<p style=\"background-image: file.jpg;\">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Why do we use it? It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>",
+          "exportId": "page-1"
         },
         {
-          "id": 33,
+          "id": 6,
           "title": "Not graded assignment",
           "type": "Assignment",
           "indent": 0,
+          "locked": false,
           "submissionTypes": null,
           "graded": false,
           "dueAt": null,
           "lockAt": null,
           "unlockAt": null,
           "requirement": null,
-          "locked": false,
           "completed": false,
-          "content": ""
+          "content": "",
+          "exportId": "i6b4f79c974dbc18ca84a97e25829bc5d"
         },
         {
-          "id": 34,
+          "id": 7,
           "title": "No Submission",
           "type": "Assignment",
           "indent": 0,
+          "locked": false,
           "submissionTypes": null,
           "graded": true,
           "pointsPossible": 0.0,
@@ -167,42 +174,69 @@ window.COURSE_DATA = {
           "lockAt": null,
           "unlockAt": null,
           "requirement": null,
-          "locked": false,
           "completed": false,
-          "content": ""
+          "content": "",
+          "exportId": "i310cba275dc3f4aa8a3306bbbe380979"
         },
         {
-          "id": 35,
+          "id": 8,
           "title": "!@#$%^&*().txt",
           "type": "Attachment",
           "indent": 0,
-          "requirement": null,
           "locked": false,
+          "requirement": null,
           "completed": false,
-          "content": "Test-Course-1-2017-Feb-16_20-02-49/viewer/files//Folder#1/1485469559_710__!@#$%^&*().txt"
+          "content": "viewer/files/Folder#1/!@#$%^&*().txt"
+        },
+        {
+          "id": 24,
+          "title": "Discussion 3",
+          "type": "DiscussionTopic",
+          "indent": 0,
+          "locked": false,
+          "graded": false,
+          "dueAt": null,
+          "lockAt": null,
+          "unlockAt": null,
+          "requirement": null,
+          "completed": false,
+          "content": "<p>stuff</p>",
+          "exportId": "icf39d29fd7924bb6b2ede5bfa34403f7"
         }
       ]
     },
     {
-      "id": 6,
+      "id": 2,
       "name": "Module 2",
       "status": "locked",
       "unlockDate": "2017-03-31T00:00:00-06:00",
       "prereqs": [],
       "requirement": "all",
       "sequential": true,
+      "exportId": "i714a4b66e3d186484eda1bfd50bf0ced",
       "items": [
         {
-          "id": 17,
-          "title": "Text Header",
-          "type": "ContextModuleSubHeader",
-          "indent": 0
+          "id": 18,
+          "title": "2-7671.jpg",
+          "type": "Attachment",
+          "indent": 0,
+          "locked": true,
+          "requirement": null,
+          "completed": false
         },
         {
-          "id": 18,
+          "id": 9,
+          "title": "Text Header",
+          "type": "ContextModuleSubHeader",
+          "indent": 0,
+          "locked": true
+        },
+        {
+          "id": 20,
           "title": "Quiz 1",
           "type": "Quizzes::Quiz",
           "indent": 0,
+          "locked": true,
           "questionCount": 0,
           "timeLimit": null,
           "attempts": 1,
@@ -213,14 +247,15 @@ window.COURSE_DATA = {
           "unlockAt": null,
           "requirement": "min_score",
           "requiredPoints": 4.0,
-          "locked": true,
-          "completed": false
+          "completed": false,
+          "exportId": "i071e7766358493f20d74567ea679ab04"
         },
         {
-          "id": 30,
+          "id": 21,
           "title": "Graded Survey",
           "type": "Quizzes::Quiz",
           "indent": 0,
+          "locked": true,
           "questionCount": 1,
           "timeLimit": null,
           "attempts": 1,
@@ -230,14 +265,15 @@ window.COURSE_DATA = {
           "lockAt": null,
           "unlockAt": null,
           "requirement": null,
-          "locked": true,
-          "completed": false
+          "completed": false,
+          "exportId": "ie09bc528e6ecd1cb9ebfafd940c20215"
         },
         {
-          "id": 31,
+          "id": 22,
           "title": "Practice Quiz",
           "type": "Quizzes::Quiz",
           "indent": 0,
+          "locked": true,
           "questionCount": 0,
           "timeLimit": null,
           "attempts": 1,
@@ -247,14 +283,15 @@ window.COURSE_DATA = {
           "lockAt": null,
           "unlockAt": null,
           "requirement": null,
-          "locked": true,
-          "completed": false
+          "completed": false,
+          "exportId": "ie797010092a6c1798006e216ba4c9c90"
         },
         {
-          "id": 32,
+          "id": 23,
           "title": "Ungraded Survey",
           "type": "Quizzes::Quiz",
           "indent": 0,
+          "locked": true,
           "questionCount": 0,
           "timeLimit": null,
           "attempts": 1,
@@ -263,28 +300,30 @@ window.COURSE_DATA = {
           "lockAt": null,
           "unlockAt": null,
           "requirement": null,
-          "locked": true,
-          "completed": false
+          "completed": false,
+          "exportId": "i413cfe4f1490c63713f7a15fea3aa1fe"
         }
       ]
     },
     {
-      "id": 7,
+      "id": 4,
       "name": "Module 3",
       "status": "locked",
       "unlockDate": null,
       "prereqs": [
-        2,
-        6
+        1,
+        2
       ],
       "requirement": "one",
       "sequential": false,
+      "exportId": "ie4c0f98c74a5ff6c274d1a2c169877c6",
       "items": [
         {
-          "id": 26,
+          "id": 12,
           "title": "Discussion 2",
           "type": "DiscussionTopic",
           "indent": 0,
+          "locked": true,
           "graded": true,
           "pointsPossible": null,
           "dueAt": null,
@@ -292,8 +331,30 @@ window.COURSE_DATA = {
           "unlockAt": null,
           "requirement": "min_score",
           "requiredPoints": 4.0,
-          "locked": true,
-          "completed": false
+          "completed": false,
+          "exportId": "ibbb015ec7bc96eade4c64ae68cb21494"
+        }
+      ]
+    },
+    {
+      "id": 7,
+      "name": "Module 5",
+      "status": "completed",
+      "unlockDate": null,
+      "prereqs": [],
+      "requirement": null,
+      "sequential": false,
+      "exportId": "i5d62ac12f9563c13413288c4a4578c87",
+      "items": [
+        {
+          "id": 16,
+          "title": "test.pdf",
+          "type": "Attachment",
+          "indent": 0,
+          "locked": false,
+          "requirement": null,
+          "completed": false,
+          "content": "viewer/files/Folder#1/Folder 2/test.pdf"
         }
       ]
     }
